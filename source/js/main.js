63c78e1b901b1fe138c046f0b8a58761563db882
miumio <nickolskaya@gmail.com>
2022-07-30 15:46:59 +0300

    feat: add intro

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {initCustomSelect} from './modules/form/init-custom-select';
 import {initFormValidate} from './modules/form/init-form-validate';
+import { initSwipers } from './modules/init-swiper';
 
 // ---------------------------------
 
@@ -11,6 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   iosVhFix();
+  initSwipers();
 
   // Modules
   // ---------------------------------
---
+++ source/js/modules/init-swiper.js
@@ -0,0 +1,30 @@
+export const initSwipers = () => {
+  // eslint-disable-next-line
+  let swiper = [].concat(new Swiper('[data-swiper]', {
+    speed: 1000,
+    loop: true,
+    direction: 'horizontal',
+    effect: "fade",
+    a11y: {
+      prevSlideMessage: 'Предыдущий слайд',
+      nextSlideMessage: 'Следующий слайд',
+    },
+    navigation: {
+      nextEl: '[data-swiper] .swiper-button-next',
+      prevEl: '[data-swiper] .swiper-button-prev',
+    },
+    autoplay: {
+      delay: 4000,
+      waitForTransition: true,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      // eslint-disable-next-line
+      renderBullet: function (index, className) {
+        return `<button type="button" aria-label="перейти к слайду №${index + 1}" class=${className}></button>`;
+      },
+    },
+  }));
+};
